d0cab237d9b58e9cd3a34a4b39cb03811a7d9a98
lilei1 <llei@umn.edu>
2021-10-06 15:43:33 -0700

    add more scripts

---
+++ scripts/.ipynb_checkpoints/orgnized_syn_d_HISNPs_variants-checkpoint.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+##by Li Lei, 2021-07-18, El Cerrito;
+#this is to orgnized the full datasets for both dSNPs and high impact variants
+#You need have both files from the dSNPs and high impact variants
+#usage: 
+use strict;
+use warnings;
+use Data::Dumper;
+
+my ($file1,$file2 )= @ARGV;
+
+my %hash; #define a hash to store the positions for each SNP;
+my $total =0;
+open(OUT,  "$file1") or die "Could not open $file1";
+my $header = <OUT>;
+#print "$header";
+foreach my $row (<OUT>){
+        chomp $row;
+        my @rtemp = split(/\t/,$row);
+       $hash{$rtemp[0]} = $row;
+ }
+close (OUT);
+
+open(FILE,  "$file2") or die "Could not open $file2";
+my $header1 = <FILE>;
+#print "$header";
+foreach my $row (<FILE>){
+        chomp $row;
+        my @tt = split(/\t/,$row);
+        if (exists $hash {$tt[0]}){
+            my $value = $hash {$tt[0]};
+            my @arr = split(/\t/,$value);
+            shift @arr; 
+            print "$row\t";
+            foreach my $ele (@arr) {
+                print "$ele\t";
+            }
+            print "\n";
+        }
+        else{
+            print "$row\t0\tNone\t0\tNone\n";
+        }
+        
+ }
+close (FILE);
+#    print "$count\n";
---
+++ scripts/core_shell_dSNP/count_synSNPs_perGene_nat.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+##by Li Lei, 2021-09-28, Alameda;
+#this is to count the variants, SNPs, small indels in files
+#You need have combined plates data
+#usage: 
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $file1 = $ARGV[0];
+
+my %hash; #define a hash to store the positions for each SNP;
+open(OUT,  "$file1") or die "Could not open $file1";
+#my $header = <OUT>;
+#print "$header";
+foreach my $row (<OUT>){
+        chomp $row;
+        my @rtemp = split(/\t/,$row);
+        push @{$hash{$rtemp[4]}}, $rtemp[0];
+ }
+close (OUT);
+#print Dumper(\%hash);
+my $count = 0;
+print "GeneID\tsyn_NB\n";
+foreach my $key (keys %hash){
+        my @array = @{$hash{$key}};
+            $count = $#array + 1;
+    print "$key\t$count\t";
+    foreach my $ele (@array) {
+        print "$ele,";
+    }
+    print "\n";
+}
